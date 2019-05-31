d1cddca602b7ea77a7a5496fbc9c8f73bd11aa65
hatle$ <kayla.hatle@yahoo.com>
2019-05-31 11:58:43 -0600

    initial commit

---
+++ src/com/skilldistillery/blackjack/Deck.java
@@ -0,0 +1,22 @@
+package com.skilldistillery.blackjack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Deck {
+	
+	
+	public Deck() {
+		
+	}
+
+	List<Card> list = new ArrayList<Card>() {
+		for
+	}
+	
+
+
+	public Deck(List<Card> list) {
+		super();
+		this.list = list;
+	}}
